756b9ca1b82812beb6eebcdf115cf4d2e0afcf2b
Hval <marianne.hval@nav.no>
2020-07-07 10:13:22 +0200

    Datovalg wip

---
+++ src/components/begrunnelse/datoValg.tsx
@@ -0,0 +1,5 @@
+export const datoValg = [
+    { label: 'Jeg klager på siste vedtak i saken', value: 'sisteVedtak', id: 'sisteVedtak' },
+    { label: 'Jeg klager på et tidligere vedtak i saken', value: 'tidligereVedtak', id: 'tidligereVedtak' },
+    { label: 'Jeg er usikker på hvilket vedtak jeg klager på ', value: 'usikkerVedtak', id: 'usikkerVedtak' }
+];
