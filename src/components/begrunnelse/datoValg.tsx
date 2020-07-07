12b5a110268521e1b23c5ef3ab9eb019e577636d
Hval <marianne.hval@nav.no>
2020-07-07 10:40:43 +0200

    Remove radio button

--- src/components/begrunnelse/datoValg.tsx
+++ src/components/begrunnelse/datoValg.tsx
@@ -1,5 +1,4 @@
 export const datoValg = [
-    { label: 'Jeg klager på siste vedtak i saken', value: 'sisteVedtak', id: 'sisteVedtak' },
-    { label: 'Jeg klager på et tidligere vedtak i saken', value: 'tidligereVedtak', id: 'tidligereVedtak' },
-    { label: 'Jeg er usikker på hvilket vedtak jeg klager på ', value: 'usikkerVedtak', id: 'usikkerVedtak' }
+    { label: 'Jeg klager på siste vedtak', value: 'sisteVedtak', id: 'sisteVedtak' },
+    { label: 'Jeg klager på et tidligere vedtak', value: 'tidligereVedtak', id: 'tidligereVedtak' }
 ];
