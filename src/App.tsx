c24174c59f5fe5a14acfb6d9f0eb38376ad0ccf2
Christian Skrøvseth <christian.skrovseth@nav.no>
2020-10-15 16:30:07 +0200

    Use JSX routing and backwards compatible root path

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import './App.less';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { routesConfig } from './utils/routes.config';
 import Layout from './components/general/layout';
 import { withRouter } from 'react-router-dom';
+import FormLandingPage from './pages/form-landing-page/form-landing-page';
+import KvitteringPage from './pages/kvittering/kvittering-page';
+import KlageEllerAnkeTema from './components/klage-eller-anke/klage-eller-anke-tema';
+import KlageEllerAnkeYtelse from './components/klage-eller-anke/klage-eller-anke-ytelse';
+import KlageEllerAnkeInnsending from './components/klage-eller-anke/klage-eller-anke-innsending';
+import NotFoundPage from './pages/not-found/not-found-page';
 
 const App = () => (
     <Router>
         <Layout>
             <Switch>
-                {routesConfig.map(route => {
-                    return <Route key={route.path} {...route} />;
-                })}
+                <Route path={['/', '/klage']} component={FormLandingPage} exact />
+                <Route path={'/oppsummering'} component={FormLandingPage} exact />
+                <Route path={'/kvittering'} component={KvitteringPage} exact />
+
+                <Route path={'/klage-anke'} component={KlageEllerAnkeTema} exact />
+                <Route path={['/klage-anke/:kategori', '/:kategori']} component={KlageEllerAnkeYtelse} exact />
+                <Route
+                    path={['/klage-anke/:kategori/:tema', '/:kategori/:tema']}
+                    component={KlageEllerAnkeInnsending}
+                    exact
+                />
+
+                <Route component={NotFoundPage} />
             </Switch>
         </Layout>
     </Router>
--- src/utils/routes.config.tsx
+++ src/utils/routes.config.tsx
@@ -1,21 +1,6 @@
 import BegrunnelsePage from '../pages/begrunnelse/begrunnelse-page';
-import FormLandingPage from '../pages/form-landing-page/form-landing-page';
 import OppsummeringSkjemaPage from '../pages/oppsummering-skjema-page/oppsummering-skjema-page';
-import DummyRedirectPage from '../pages/dummy-redirect/dummy-redirect-page';
-import KvitteringPage from '../pages/kvittering/kvittering-page';
-import NotFoundPage from '../pages/not-found/not-found-page';
 import { JSXElementConstructor } from 'react';
-import KlageEllerAnkeYtelse from '../components/klage-eller-anke/klage-eller-anke-ytelse';
-import KlageEllerAnkeTema from '../components/klage-eller-anke/klage-eller-anke-tema';
-import KlageEllerAnkeInnsending from '../components/klage-eller-anke/klage-eller-anke-innsending';
-
-export type RouteType = {
-    step?: number;
-    path: string;
-    component: JSXElementConstructor<any>;
-    redirect?: string;
-    exact: boolean;
-};
 
 export type FormStep = {
     step?: number;
@@ -42,48 +27,3 @@ export const formSteps: FormStep[] = [
         exact: true
     }
 ];
-
-export const routesPages: RouteType[] = [
-    {
-        path: `/`,
-        component: KlageEllerAnkeTema,
-        exact: true
-    },
-    {
-        path: `/redirect`,
-        component: DummyRedirectPage,
-        exact: true
-    },
-    {
-        path: `/kvittering`,
-        component: KvitteringPage,
-        exact: true
-    },
-    {
-        path: `/klage`,
-        component: FormLandingPage,
-        exact: true
-    },
-    {
-        path: `/oppsummering`,
-        component: FormLandingPage,
-        exact: true
-    },
-    {
-        path: `/:kategori`,
-        component: KlageEllerAnkeYtelse,
-        exact: true
-    },
-    {
-        path: `/:kategori/:tema`,
-        component: KlageEllerAnkeInnsending,
-        exact: true
-    },
-    {
-        path: `*`,
-        component: NotFoundPage,
-        exact: true
-    }
-];
-
-export const routesConfig = routesPages;
