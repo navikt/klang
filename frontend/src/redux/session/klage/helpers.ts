087bf2f93cfd16f7a3c1574dd49782b8d3dcb5d8
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-05-31 09:16:33 +0200

    Add fallback ID function for crypto.randomUUID

--- frontend/src/redux/session/klage/helpers.ts
+++ frontend/src/redux/session/klage/helpers.ts
@@ -10,7 +10,7 @@ export const createSessionCase = (
   innsendingsytelse: Innsendingsytelse,
   internalSaksnummer: string | null,
 ): ISessionCase => ({
-  id: crypto.randomUUID(),
+  id: getId(),
   type,
   innsendingsytelse,
   foedselsnummer: '',
@@ -27,3 +27,19 @@ export const createSessionCase = (
   modifiedByUser: new Date().toISOString(),
   caseIsAtKA: null,
 });
+
+const SUPPORTS_CRYPTO =
+  'crypto' in window &&
+  typeof window.crypto === 'object' &&
+  window.crypto !== null &&
+  'getRandomValues' in window.crypto &&
+  typeof window.crypto.getRandomValues === 'function';
+
+const fallbackIdGenerator = (): `${string}-${string}` => {
+  const now = new Date().getTime();
+  const random = Math.random().toString(36).substring(2);
+
+  return `${now}-${random}`;
+};
+
+const getId = SUPPORTS_CRYPTO ? () => window.crypto.randomUUID() : () => fallbackIdGenerator();
