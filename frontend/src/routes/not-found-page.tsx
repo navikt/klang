d9fd6d9bf114886f63d3ac6fa297b8f84eaeb9b3
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-10-01 13:54:02 +0200

    Simplify not found page logic

--- frontend/src/routes/not-found-page.tsx
+++ frontend/src/routes/not-found-page.tsx
@@ -1,15 +1,8 @@
-import { ENVIRONMENT } from '@app/environment/environment';
 import { redirectToNav } from '@app/functions/redirect-to-nav';
 import { useEffect } from 'react';
 
 export const NotFoundPage = () => {
-  useEffect(() => {
-    console.warn('404 - Redirecting to www.nav.no/klage');
-
-    if (!ENVIRONMENT.isLocal) {
-      redirectToNav();
-    }
-  }, []);
+  useEffect(redirectToNav, []);
 
   return null;
 };
