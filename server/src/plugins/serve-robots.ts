cdc804ab529d74241b01b1c894e9e52a7b5254e0
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-11-11 11:19:05 +0100

    Serve robots.txt from root

---
+++ server/src/plugins/serve-robots.ts
@@ -0,0 +1,12 @@
+import fastifyPlugin from 'fastify-plugin';
+
+export const SERVE_ROBOTS_PLUGIN_ID = 'serve-robots';
+
+export const serveRobotsPlugin = fastifyPlugin(
+  async (app) => {
+    app.get('/robots.txt', async (_, res) => {
+      res.redirect('https://cdn.nav.no/klage/klang/assets/robots.txt', 301);
+    });
+  },
+  { fastify: '5', name: SERVE_ROBOTS_PLUGIN_ID },
+);
