276235049b5c899e2f25e71e3a20245d1d1e3f61
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-12-03 09:33:15 +0100

    Fix ERR_HTTP_HEADERS_SENT

--- server/src/plugins/serve-robots.ts
+++ server/src/plugins/serve-robots.ts
@@ -4,9 +4,7 @@ export const SERVE_ROBOTS_PLUGIN_ID = 'serve-robots';
 
 export const serveRobotsPlugin = fastifyPlugin(
   async (app) => {
-    app.get('/robots.txt', async (_, res) => {
-      res.redirect('https://cdn.nav.no/klage/klang/assets/robots.txt', 301);
-    });
+    app.get('/robots.txt', async (_, reply) => reply.redirect('https://cdn.nav.no/klage/klang/assets/robots.txt', 301));
   },
   { fastify: '5', name: SERVE_ROBOTS_PLUGIN_ID },
 );
