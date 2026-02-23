ede9a9d942a48b6d4cd94a53092421f8ac23d789
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2026-02-23 16:01:09 +0100

    Catch file size errors client side

--- frontend/src/components/attachments/upload-button.tsx
+++ frontend/src/components/attachments/upload-button.tsx
@@ -1,23 +1,34 @@
 import { isError } from '@app/functions/is-api-error';
+import { ErrorMessageKeys } from '@app/language/error-messages';
 import { useTranslation } from '@app/language/use-translation';
 import { AppEventEnum } from '@app/logging/action';
 import { appEvent } from '@app/logging/logger';
 import { useUploadAttachmentMutation } from '@app/redux-api/case/api';
+import type { Attachment } from '@app/redux-api/case/types';
 import { UploadIcon } from '@navikt/aksel-icons';
 import { Button } from '@navikt/ds-react';
 import type { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 import { useRef } from 'react';
+
+const BYTES_PER_KB = 1_024;
+const BYTES_PER_MIB = BYTES_PER_KB * BYTES_PER_KB;
+const MAX_SIZE_MIB_SINGLE = 8;
+const MAX_SIZE_MIB_TOTAL = 32;
+const OVERHEAD_BYTES = 288;
+const MAX_SIZE_BYTES_SINGLE = MAX_SIZE_MIB_SINGLE * BYTES_PER_MIB - OVERHEAD_BYTES;
+const MAX_SIZE_BYTES_TOTAL = MAX_SIZE_MIB_TOTAL * BYTES_PER_MIB;
 
 interface Props {
   caseId: string;
   inputId: string;
   setLoading: (loading: boolean) => void;
   isLoading: boolean;
-  addError: (error: FetchBaseQueryError) => void;
+  addError: (error: FetchBaseQueryError | string) => void;
+  attachments: Attachment[];
 }
 
-export const UploadButton = ({ inputId, setLoading, isLoading, addError, caseId }: Props) => {
-  const { skjema } = useTranslation();
+export const UploadButton = ({ inputId, setLoading, isLoading, addError, caseId, attachments }: Props) => {
+  const { skjema, error_messages } = useTranslation();
   const fileInput = useRef<HTMLInputElement>(null);
   const [uploadAttachment] = useUploadAttachmentMutation();
 
@@ -35,6 +46,25 @@ export const UploadButton = ({ inputId, setLoading, isLoading, addError, caseId 
     const { files } = event.target;
 
     if (files === null || files.length === 0) {
+      return;
+    }
+
+    let pickedSize = 0;
+
+    for (const file of files) {
+      pickedSize += file.size;
+
+      if (file.size > MAX_SIZE_BYTES_SINGLE) {
+        addError(`${file.name}: ${error_messages[ErrorMessageKeys.TOO_LARGE]}`);
+        return;
+      }
+    }
+
+    const uploadedSize = attachments.reduce((total, attachment) => total + attachment.sizeInBytes, 0);
+    const overhead = files.length * OVERHEAD_BYTES;
+
+    if (uploadedSize + pickedSize + overhead > MAX_SIZE_BYTES_TOTAL) {
+      addError(error_messages[ErrorMessageKeys.TOTAL_TOO_LARGE]);
       return;
     }
 
