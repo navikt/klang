d35829de8d8b9302d70b940b3ba3c2f9b975ea48
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2026-04-17 08:21:24 +0200

    Update dependencies

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -2,7 +2,6 @@ import { reduxStore } from '@app/redux/configure-store';
 import { Router } from '@app/routes/routes';
 import { StrictMode } from 'react';
 import { Provider } from 'react-redux';
-import '../global.css';
 
 export const App = () => (
   <StrictMode>
