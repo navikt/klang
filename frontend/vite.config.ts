f5507ff3454ced51ac90f75702a2d152c34e554e
Christian Skrøvseth <3177301+cskrov@users.noreply.github.com>
2026-09-09 11:07:19 +0200

    Update dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   base: 'https://cdn.nav.no/klage/klang',
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: {
     port: 8064,
     proxy: {
